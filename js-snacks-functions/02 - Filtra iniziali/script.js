1d0792864798e8a7183f4c15438e1442f7f51a27
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-04 15:46:10 +0100

    feat: completata parte del secondo esercizio

--- js-snacks-functions/02 - Filtra iniziali/script.js
+++ js-snacks-functions/02 - Filtra iniziali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
-
+//dichiarazione di variabili
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const solamenteA = onlyA (names, "A")
 
 // Dichiara la funzione qui.
-
+function onlyA (names, letter){
+    let solamenteA=[]
+    for(i=0; i < names.length; i++){
+        
+        if(names[i][0] == letter ){
+            solamenteA.push(names[i])
+        }
+        return solamenteA;
+        
+    
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(solamenteA)
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
